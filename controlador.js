7c8a81f4b524f173ba4bcb11a043f37ae5670f67
Juan <juan.gallegomesa@gmail.com>
2020-09-26 16:13:28 -0500

    se consume servicio POST

--- controlador.js
+++ controlador.js
@@ -1,10 +1,42 @@
-const url = "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU/top-tracks?country=US";
-const token = "Bearer BQCTmfpH-0ZjcutSvhLRfhEyedcMLErgEv9NL0QnattqhuhiDP853W8-OkgqA9kmub8dSLReFfn-VALBt27sFs3h5-iMEhEu51yfU4_JW4qTsZhMmpkMpjxOLEgYRTlCD0F5asw-pWzRWBhCPfY";
+const llave1 = "grant_type=client_credentials";
+const llave2 = "client_id=f5d8c0020ca64dc7a2fdbf1d858869c0";
+const llave3 = "client_secret=53ce8d86a24946628aa808f91529f858";
+
+const parametrosPOST = {
+    method: "POST",
+    headers: { "Content-Type": 'application/x-www-form-urlencoded' },
+    body: llave1 + '&' + llave2 + '&' + llave3
+}
+
+const urlPOST = "https://accounts.spotify.com/api/token";
+
+fetch(urlPOST, parametrosPOST)
+    .then(respuesta => respuesta.json())
+    .then(datos => generarToken(datos));
+
+
+function generarToken(datos) {
+
+    let token = datos.access_token;
+    token = "Bearer " + token;
+
+
+
+
+}
+
+
+
+
+/*const url = "https://api.spotify.com/v1/artists/4gzpq5DPGxSnKTe4SA8HAU/top-tracks?country=US";
+
 
 const parametros = {
     method: "GET",
     headers: { "Authorization": token },
 }
+
+
 
 fetch(url, parametros)
     .then(respuesta => respuesta.json())
@@ -13,11 +45,11 @@ fetch(url, parametros)
 
 
 function depurarDatos(datos) {
-    /*console.log(datos); //objeto (tracks)
+    console.log(datos); //objeto (tracks)
     console.log(datos.tracks); //arreglo (10 posiciones)
-    console.log(datos.tracks[0]); //primer objeto de 10 posibles*/
+    console.log(datos.tracks[0]); //primer objeto de 10 posibles
     console.log(datos.tracks[0].preview_url);
     console.log(datos.tracks[0].name);
 
 
-}
+}*/
